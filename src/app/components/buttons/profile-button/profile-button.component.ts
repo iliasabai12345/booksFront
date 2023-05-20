43734e3a101acd03823a7bc9c2554cc2806dcea9
iliasabai123 <ilias.abai.alser@gmail.com>
2023-05-20 21:49:15 +0600

    add button menu

--- src/app/components/buttons/profile-button/profile-button.component.ts
+++ src/app/components/buttons/profile-button/profile-button.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {StorageService} from "shared/services/storage.service";
 
 @Component({
   selector: 'app-profile-button',
@@ -6,5 +7,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./profile-button.component.scss']
 })
 export class ProfileButtonComponent {
+  constructor(private readonly storageService: StorageService) {
+  }
 
+  get buttons(): any[] {
+    return [
+      {
+        id: 1, title: 'Войти', title_kk: '', icon: 'login', show: !this.isAuthorized, action: () => {
+        }
+      },
+      {
+        id: 2, title: 'Регистрация', title_kk: '', icon: 'logout', show: !this.isAuthorized, action: () => {
+        }
+      },
+      {
+        id: 3, title: 'Контакты', title_kk: '', icon: 'contacts', show: true, action: () => {
+        }
+      },
+      {
+        id: 4, title: 'Мои заказы', title_kk: '', icon: 'list_alt', show: this.isAuthorized, action: () => {
+        }
+      },
+    ]
+  }
+
+  // Проверка на авторизацию пользователя
+  get isAuthorized(): boolean {
+    return !!this.storageService.user;
+  }
 }
