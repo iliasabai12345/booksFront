942e6da7684b640799b95a11a168b59ebe7e88c1
iliasabai123 <ilias.abai.alser@gmail.com>
2023-05-22 23:38:29 +0600

    add detail page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import {NgModule} from '@angular/core';
 import {MatProgressBarModule} from "@angular/material/progress-bar";
 import {BrowserModule} from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {AddCartBtnModule} from "src/app/components/buttons/add-cart-btn/add-cart-btn.module";
 import {FooterModule} from "src/app/components/footer/footer.module";
 import {HeaderModule} from "src/app/components/header/header.module";
 import {ProductCardModule} from "src/app/components/product-card/product-card.module";
@@ -18,17 +19,18 @@ import { ProductDetailComponent } from './pages/product-detail/product-detail.co
     CategoryComponent,
     ProductDetailComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    MainModule,
-    BrowserAnimationsModule,
-    HeaderModule,
-    FooterModule,
-    ProductCardModule,
-    MatProgressBarModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        HttpClientModule,
+        MainModule,
+        BrowserAnimationsModule,
+        HeaderModule,
+        FooterModule,
+        ProductCardModule,
+        MatProgressBarModule,
+        AddCartBtnModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
